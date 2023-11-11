ccf56aa4f4cf300739bee0b22f76c24bdb614c94
22glf1 <glf1@williams.edu>
2023-11-11 17:38:24 -0500

    updates useFetch event and events

--- apps/web/src/hooks/useFetchEvent.tsx
+++ apps/web/src/hooks/useFetchEvent.tsx
@@ -2,12 +2,12 @@ import { EventType } from "@/types/Event";
 import useSWR from "swr";
 
 export function useFetchEvent(index: number): {
-  event: EventType;
+  event: any;
   isError: boolean;
   isLoading: boolean;
 } {
-  const fetcher = (...args: any[]) => fetch(...args).then((res) => res.json());
-  const { data, error, isLoading } = useSWR("/api/events", fetcher);
+  // const fetcher = (...args: any[]) => fetch(...args).then((res) => res.json());
+  // const { data, error, isLoading } = useSWR("/api/events", fetcher);
 
-  return { event: data[index], isError: error, isLoading };
+  return { event: {}, isError: false, isLoading: false };
 }
