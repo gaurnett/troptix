8f83cb182fcc9a70e5fda0ac6fbda82b32164b8a
22glf1 <glf1@williams.edu>
2023-11-12 19:20:25 -0500

    Changes favicon and title

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -14,7 +14,7 @@ function App({ Component, pageProps, router }: AppProps) {
   return (
     <>
       <Head>
-        <title>Troptix</title>
+        <title>TropTix</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
