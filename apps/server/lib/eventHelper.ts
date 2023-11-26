17e12a20599ad628b194b33a6e5d50d5582d21c1
22glf1 <glf1@williams.edu>
2023-11-25 19:24:41 -0500

    Updates event and ticket types start/end time

--- apps/server/lib/eventHelper.ts
+++ apps/server/lib/eventHelper.ts
@@ -9,9 +9,7 @@ export function getPrismaTicketTypeQuery(ticket) {
     maxPurchasePerUser: ticket.maxPurchasePerUser,
     quantity: ticket.quantity,
     saleStartDate: ticket.saleStartDate,
-    saleStartTime: ticket.saleStartTime,
     saleEndDate: ticket.saleEndDate,
-    saleEndTime: ticket.saleEndTime,
     price: ticket.price,
     ticketingFees: ticket.ticketingFees,
     event: {
@@ -35,9 +33,7 @@ export function getPrismaUpdateEventQuery(event) {
     organizer: event.organizer,
     organizerUserId: event.organizerUserId,
     endDate: event.endDate,
-    endTime: event.endTime,
     startDate: event.startDate,
-    startTime: event.startTime,
     venue: event.venue,
     address: event.address,
     countryCode: event.country_code,
@@ -59,61 +55,13 @@ export function getPrismaCreateStarterEventQuery(event) {
     organizer: event.organizer,
     organizerUserId: event.organizerUserId,
     endDate: event.endDate,
-    endTime: event.endTime,
     startDate: event.startDate,
-    startTime: event.startTime,
     venue: event.venue,
     address: event.address,
     countryCode: event.country_code,
     latitude: event.latitude,
     longitude: event.longitude,
     country: event.country,
-  }
-
-  return eventInput;
-}
-
-export function getPrismaCreateEventQuery(event) {
-  let eventInput: Prisma.EventsCreateInput;
-  let ticketTypes: Prisma.TicketTypesCreateManyEventInput[] = [];
-
-  for (const ticketType of event.ticketTypes) {
-    ticketTypes.push({
-      name: ticketType.name,
-      description: ticketType.description,
-      maxPurchasePerUser: ticketType.maxPurchasePerUser,
-      quantity: ticketType.quantity,
-      saleStartDate: ticketType.saleStartDate,
-      saleStartTime: ticketType.saleStartTime,
-      saleEndDate: ticketType.saleEndDate,
-      saleEndTime: ticketType.saleEndTime,
-      price: ticketType.price
-    })
-  }
-
-  eventInput = {
-    id: event.id,
-    imageUrl: event.imageUrl,
-    name: event.name,
-    description: event.description,
-    summary: event.summary,
-    organizer: event.organizer,
-    organizerUserId: event.organizerUserId,
-    endDate: event.endDate,
-    endTime: event.endTime,
-    startDate: event.startDate,
-    startTime: event.startTime,
-    venue: event.venue,
-    address: event.address,
-    countryCode: event.country_code,
-    latitude: event.latitude,
-    longitude: event.longitude,
-    country: event.country,
-    ticketTypes: {
-      createMany: {
-        data: ticketTypes
-      }
-    }
   }
 
   return eventInput;
