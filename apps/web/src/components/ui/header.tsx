c512cc54bcfa78336e36cfb823f5f0d4e36cff54
22glf1 <glf1@williams.edu>
2023-11-27 10:57:30 -0500

    Updates header to use white background

--- apps/web/src/components/ui/header.tsx
+++ apps/web/src/components/ui/header.tsx
@@ -70,7 +70,7 @@ export default function Header() {
   }
 
   return (
-    <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top ? 'bg-white backdrop-blur-sm shadow-lg' : ''} ${pathname === "/event" ? 'bg-white bg-opacity-80' : ""}`}>
+    <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top ? 'bg-white backdrop-blur-sm shadow-lg' : ''} ${pathname === "/event" ? 'bg-white' : ""}`}>
       <div className="max-w-6xl mx-auto px-5 sm:px-6">
         <div className="flex items-center justify-between h-24 md:h-28">
           <div className="shrink-0 mr-4">
