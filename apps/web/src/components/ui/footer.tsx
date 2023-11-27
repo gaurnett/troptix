4dc33abad9cab1a497912813ad06798c95564c3b
22glf1 <glf1@williams.edu>
2023-11-27 12:19:01 -0500

    Updates footer

--- apps/web/src/components/ui/footer.tsx
+++ apps/web/src/components/ui/footer.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Logo from './logo'
 
 export default function Footer() {
   return (
@@ -7,13 +6,10 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
 
         {/* Top area: Blocks */}
-        <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
+        <div className="grid sm:grid-cols-11 gap-8 pb-8 md:py-12 border-t border-gray-200">
 
           {/* 1st block */}
-          <div className="sm:col-span-12 lg:col-span-3">
-            <div className="mb-2">
-              <Logo />
-            </div>
+          <div className=" sm:col-span-12 lg:col-span-3">
           </div>
 
           {/* 2nd block */}
