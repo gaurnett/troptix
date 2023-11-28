8b8cba195e360eaa89dea209d2483feae1f63a22
22glf1 <glf1@williams.edu>
2023-11-28 15:16:14 -0500

    Updates ticket page

--- apps/web/src/pages/tickets/index.tsx
+++ apps/web/src/pages/tickets/index.tsx
@@ -1,43 +1,30 @@
 import { Spinner } from "@/components/ui/spinner";
+import { GetOrdersType, useFetchOrderById } from "@/hooks/useOrders";
 import { Button, Carousel, Divider, QRCode } from "antd";
 import { format } from 'date-fns';
 import JsPDF from 'jspdf';
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
 import {
   MdOutlineKeyboardArrowLeft,
   MdOutlineKeyboardArrowRight,
 } from "react-icons/md";
-import { GetOrdersType, getOrders } from 'troptix-api';
 
 export default function TicketsPage() {
   const router = useRouter();
   const carouselRef = useRef<any>();
-  const orderId = router.query.orderId;
-  const [isFetchingOrders, setIsFetchingOrders] = useState(true);
-  const [order, setOrder] = useState<any>();
+  const orderId = router.query.orderId as string;
 
-  useEffect(() => {
-    async function fetchOrders() {
-      try {
-        const getOrdersRequest: any = {
-          getOrdersType: GetOrdersType.GET_ORDER_BY_ID,
-          orderId: orderId
-        }
-        const response = await getOrders(getOrdersRequest);
-
-        if (response !== undefined) {
-          setOrder(response);
-        }
-      } catch (error) {
-      }
-
-      setIsFetchingOrders(false);
-    };
-
-    fetchOrders();
-  }, [orderId]);
+  const {
+    isPending,
+    isError,
+    data: order,
+    error,
+  } = useFetchOrderById({
+    getOrdersType: GetOrdersType.GET_ORDER_BY_ID,
+    id: orderId,
+  });
 
   function goNext() {
     carouselRef.current.next();
@@ -122,44 +109,45 @@ export default function TicketsPage() {
 
   }
 
+  if (isPending) {
+    return (
+      <div className="mt-32">
+        <Spinner text={"Fetching Tickets"} />
+      </div>
+    )
+  }
+
   return (
     <div className="mt-32 mb-8 w-full md:max-w-3xl mx-auto">
-      {
-        isFetchingOrders ?
-          <Spinner text={"Fetching Tickets"} />
-          :
-          <div>
-            <h1 className="mx-4 text-center text-4xl md:text-4xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
-              <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">{order.event.name}</span>
-            </h1>
-            <div className='flex w-full md:max-w-lg md:mx-auto my-6 mx-auto justify-center'>
-              <Button onClick={saveToPdf} className="px-4 py-4 shadow-md items-center justify-center font-medium inline-flex">Save PDF</Button>
-              <Link target="_blank" href={{ pathname: "/event", query: { eventId: order.event.id } }}>
-                <Button className="ml-4 px-4 py-4 shadow-md items-center justify-center font-medium inline-flex">View event details</Button>
+      <div>
+        <h1 className="mx-4 text-center text-4xl md:text-4xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">{order.event.name}</span>
+        </h1>
+        <div className='flex w-full md:max-w-lg md:mx-auto my-6 mx-auto justify-center'>
+          <Button onClick={saveToPdf} className="px-4 py-4 shadow-md items-center justify-center font-medium inline-flex">Save PDF</Button>
+          <Link target="_blank" href={{ pathname: "/event", query: { eventId: order.event.id } }}>
+            <Button className="ml-4 px-4 py-4 shadow-md items-center justify-center font-medium inline-flex">View event details</Button>
 
-              </Link>
-            </div>
-            <div className="w-full md:max-w-lg mx-auto">
-              <div className="mx-auto w-full h-full">
-                <div className="flex-inline justify-center">
-                  <div className='w-full'>
-                    <Carousel ref={carouselRef} arrows={true} draggable={true}>
-                      {
-                        order.tickets.map((ticket: any, index: number) => {
-                          return (
-                            renderTicket(ticket, order.event, index, order.tickets.length)
-                          );
-                        })
-                      }
-                    </Carousel>
-                  </div>
-                </div>
+          </Link>
+        </div>
+        <div className="w-full md:max-w-lg mx-auto">
+          <div className="mx-auto w-full h-full">
+            <div className="flex-inline justify-center">
+              <div className='w-full'>
+                <Carousel ref={carouselRef} arrows={true} draggable={true}>
+                  {
+                    order.tickets.map((ticket: any, index: number) => {
+                      return (
+                        renderTicket(ticket, order.event, index, order.tickets.length)
+                      );
+                    })
+                  }
+                </Carousel>
               </div>
-
-
             </div>
           </div>
-      }
+        </div>
+      </div>
 
     </div>
   );
