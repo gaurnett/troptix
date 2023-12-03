f49b81604acb68e6785a960ba12135602df8c96f
22glf1 <glf1@williams.edu>
2023-12-03 16:41:34 -0500

    Removes EST

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getDateFormatter(date: Date) {
-  return `${formatInTimeZone(date, 'America/New_York', "MMM dd, yyyy, h:mm a")} EST`;
+  return `${formatInTimeZone(date, 'America/New_York', "MMM dd, yyyy, h:mm a")}`;
 }
 
 export function getFormattedCurrency(price) {
