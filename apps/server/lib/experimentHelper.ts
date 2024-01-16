98cdf144532731d84c5bcfb9525611bbaf1042a8
22glf1 <glf1@williams.edu>
2024-01-15 19:00:44 -0500

    Adds emmanuel@usetroptix.com

--- apps/server/lib/experimentHelper.ts
+++ apps/server/lib/experimentHelper.ts
@@ -5,6 +5,8 @@ export const adminUserIds = [
   "6Oqm1AIPI8a5nf1fQHBIPx7lJMI3", // flowersgaurnett@gmail.com (prod)
   "VMl1xR85jXfBuLDDNW4mF3NNgch1", // emmanuel.sylvester22@gmail.com (dev)
   "mUyn6nbtpZMACL7fE6y43SutZ2g1", // emmanuel.sylvester22@gmail.com (prod)
+  "Qd57H9TrFyRdfDIxeTa89IJO2Q42", // emmanuel@usetroptix.com (dev)
+  "AI1nZ9bNTuMgLDFSgKnxBWsfK4d2", // emmanuel@usetroptix.com (prod)
   "ZTrfkUzOBxfxpTJhytwSDrVSfGu1", // danielchatani@gmail.com (prod)
   "Sl3mQJwE7YTl1iVylE5FWVf1eh53", // daniel@usetroptix.com (dev)
   "h98VfETqz3TRdt9gv5pjEKpI4TZ2", // daniel@usetroptix.com (prod)
