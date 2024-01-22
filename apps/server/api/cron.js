2fd7c78f604bde7d4692da08396f50843d3adc50
Emmanuel Sylvester <emmanuel.sylvester22@gmail.com>
2024-01-22 09:54:08 -0800

    Add cron job

---
+++ apps/server/api/cron.js
@@ -0,0 +1,33 @@
+import { invalidateOrders } from '../lib/invalidateOrdersHelper';
+// import type { Request, Response } from 'express';
+
+const cronJobType = {
+  INVALIDATE_ORDERS: 'INVALIDATE_ORDERS',
+};
+
+const ORDER_EXPIRATION_LIMIT = 5;
+
+export default async function handler(request, response) {
+  try {
+    const cronJobName = request.query.cronJob;
+
+    let resultMessage;
+    switch (cronJobName) {
+      case cronJobType.INVALIDATE_ORDERS:
+        const numberOfInvalidatedOrders = await invalidateOrders(
+          ORDER_EXPIRATION_LIMIT
+        );
+        resultMessage = `${numberOfInvalidatedOrders} Expired orders invalidated successfully`;
+        break;
+      default:
+        throw new Error('Invalid cron job name');
+    }
+
+    response.status(200).json({ message: resultMessage });
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({
+      message: `An error occurred while running cron job: ${error.message}`,
+    });
+  }
+}
