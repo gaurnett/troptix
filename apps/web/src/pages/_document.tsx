e150caca4161bfe1c18046cd2fc38b6bbdb14395
Emmanuel Sylvester <emmanuel.sylvester22@gmail.com>
2024-12-27 20:58:42 -0500

    Add Logo for Apple homescreen shortcuts

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="apple-touch-icon" href="/logo_v1.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
