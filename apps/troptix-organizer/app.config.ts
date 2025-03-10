2e29295a2303bb3474ad23313e9f8e7fec657612
22glf1 <glf1@williams.edu>
2025-03-09 20:30:09 -0400

    Updated app

--- apps/troptix-organizer/app.config.ts
+++ apps/troptix-organizer/app.config.ts
@@ -5,31 +5,37 @@ const APP_ENVIRONMENT = process.env.APP_VARIANT; // let's declare variable to st
 let googleServicesPlist: string | undefined = './GoogleService-Info.plist'; // while developing this file will be the default.
 let webClientId: string | undefined =
   '912947419048-sark3aqudtojmsci3tk9c6p5ud9o7aes.apps.googleusercontent.com';
-let apiUrl = 'http://192.168.1.214:3001';
+let apiUrl = 'http://192.168.100.123:3000';
+
+// PROD PUSH
+googleServicesPlist = './GoogleService-Info-Prod.plist';
+webClientId =
+  '801810211842-jh1pm61f0lq9fvuhvrug0pnir9g2kill.apps.googleusercontent.com';
+apiUrl = 'https://usetroptix.com';
 
 // then checking which env we are, and based on that choosing
 // the right Google services file to add
 if (APP_ENVIRONMENT === 'development') {
   googleServicesPlist = process.env.GOOGLE_SERVICE_INFO_IOS_DEV;
   webClientId = process.env.WEB_CLIENT_ID;
-  apiUrl = 'https://troptix-git-dev-flowersgaurnett-gmailcom.vercel.app';
+  apiUrl = 'https://troptix-web-s5n5-git-main-troptix.vercel.app';
 } else if (APP_ENVIRONMENT === 'preview') {
   googleServicesPlist = process.env.GOOGLE_SERVICE_INFO_IOS_DEV;
   webClientId = process.env.WEB_CLIENT_ID;
-  apiUrl = 'https://troptix-git-dev-flowersgaurnett-gmailcom.vercel.app';
+  apiUrl = 'https://troptix-web-s5n5-git-main-troptix.vercel.app';
 } else if (APP_ENVIRONMENT === 'preprod') {
   googleServicesPlist = process.env.GOOGLE_SERVICE_INFO_IOS_DEV;
   webClientId = process.env.WEB_CLIENT_ID;
-  apiUrl = 'https://troptix-git-dev-flowersgaurnett-gmailcom.vercel.app';
+  apiUrl = 'https://troptix-web-s5n5-git-main-troptix.vercel.app';
 } else if (APP_ENVIRONMENT === 'prod') {
   googleServicesPlist = process.env.GOOGLE_SERVICE_INFO_IOS_PROD;
   webClientId = process.env.WEB_CLIENT_ID;
-  apiUrl = 'https://api.usetroptix.com';
+  apiUrl = 'https://usetroptix.com';
 }
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  version: '1.1.1',
+  version: '1.1.3',
   slug: 'troptix-organizer',
   icon: './assets/icon.png',
   name: process.env.APP_NAME || 'TropTix Organizer',
@@ -63,7 +69,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       },
     ],
     [
-      'expo-barcode-scanner',
+      'expo-camera',
       {
         cameraPermission:
           'Allow TropTix to access your camera to scan event tickets. This will allow you to validate tickets purchased on our platform for your events.',
@@ -79,8 +85,14 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
           buildToolsVersion: '31.0.0',
         },
         ios: {
-          deploymentTarget: '13.4',
+          deploymentTarget: '15.1',
           useFrameworks: 'static',
+          infoPlist: {
+            ITSAppUsesNonExemptEncryption: false,
+          },
+          entitlements: {
+            'aps-environment': 'production',
+          },
         },
       },
     ],
