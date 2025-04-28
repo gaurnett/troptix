dad09b73ba4c18130088af00ad3093d142069b87
Emmanuel Sylvester <38680059+emansyl@users.noreply.github.com>
2025-04-28 12:34:20 -0500

    Remove unused server code (#186)

--- apps/server/api/fileupload.ts
+++
@@ -1,8 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import { allowCors } from '../lib/auth.js';
-
-async function handler(request: VercelRequest, response: VercelResponse) {
-  return response.status(200).send('fileupload');
-}
-
-export default allowCors(handler);
