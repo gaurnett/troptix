923baa3b2b8fc6338a253feb5da3c81481e3138a
Emmanuel Sylvester <emmanuel.sylvester22@gmail.com>
2025-07-10 00:19:43 -0400

    Prevent Firebase Admin from being Init at Build

--- apps/web/src/server/lib/firebaseAdmin.ts
+++ apps/web/src/server/lib/firebaseAdmin.ts
@@ -1,14 +1,25 @@
 import * as admin from 'firebase-admin';
 
-// Only initialize if no apps exist
-if (admin.apps.length === 0) {
-  const base64 = process.env.FIREBASE_SERVICE_ACCOUNT_KEY_BASE_64! as string;
-  const json = Buffer.from(base64, 'base64').toString('utf-8');
-  const serviceAccount = JSON.parse(json);
+let initialized = false;
 
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
+function init() {
+  if (!initialized && admin.apps.length === 0) {
+    const base64 = process.env.FIREBASE_SERVICE_ACCOUNT_KEY_BASE_64;
+    if (!base64) {
+      throw new Error('Missing FIREBASE_SERVICE_ACCOUNT_KEY_BASE_64');
+    }
+
+    const json = Buffer.from(base64, 'base64').toString('utf-8');
+    const serviceAccount = JSON.parse(json);
+
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+
+    initialized = true;
+  }
 }
+
+init();
 
 export default admin;
