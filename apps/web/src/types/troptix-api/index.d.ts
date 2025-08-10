1b1020bce0c3f97d243c9f1a5d4acdd4501a1617
22glf1 <glf1@williams.edu>
2025-08-10 15:23:59 -0400

    Removes packages folder

--- apps/web/src/types/troptix-api/index.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'troptix-api';
--- packages/troptix-react-components/Test.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native-ui-lib';
-
-export default function Test() {
-  return <Text>Events</Text>;
-}
