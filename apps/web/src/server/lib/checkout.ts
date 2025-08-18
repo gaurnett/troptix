807d0f26692b31e504c3e444efc5be7769edc955
Emmanuel Sylvester <emmanuel.sylvester22@gmail.com>
2025-08-18 15:47:26 -0400

    Update fees to 8%+-zsh.50

--- apps/web/src/server/lib/checkout.ts
+++ apps/web/src/server/lib/checkout.ts
@@ -6,7 +6,7 @@
  */
 export function calculateFees(price: number) {
   if (price === 0) return 0;
-  const fee = price * 0.04 + 0.3;
+  const fee = price * 0.08 + 0.5;
   const tax = fee * 0.15;
   return parseFloat((fee + tax).toFixed(2));
 }
